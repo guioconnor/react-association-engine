f336819aff7e8d47907df7ff026433a0fb42c391
Guilherme Zühlke O'Connor <guioconnor@gmail.com>
2018-03-30 23:26:07 +0100

    Set matching results and expressions

--- src/App.js
+++ src/App.js
@@ -24,19 +24,19 @@ const Game = () => (
   <Board>
     <TargetSection>
       <TargetExpression value={4}>2 + 2</TargetExpression>
-      <TargetExpression value={4}>1 + 3</TargetExpression>
+      <TargetExpression value={8}>5 + 3</TargetExpression>
       <TargetExpression value={11}>5 + 6</TargetExpression>
-      <TargetExpression value={11}>2 + 9</TargetExpression>
-      <TargetExpression value={11}>4 + 7</TargetExpression>
+      <TargetExpression value={5}>3 + 2</TargetExpression>
+      <TargetExpression value={6}>2 + 4</TargetExpression>
       <TargetExpression value={7}>4 + 3</TargetExpression>
     </TargetSection>
     <ResultSection>
-      <DraggableResult value={4} />
-      <DraggableResult value={8} />
-      <DraggableResult value={11} />
       <DraggableResult value={5} />
-      <DraggableResult value={7} />
+      <DraggableResult value={8} />
       <DraggableResult value={6} />
+      <DraggableResult value={11} />
+      <DraggableResult value={7} />
+      <DraggableResult value={4} />
     </ResultSection>
     <CustomDragLayer snapToGrid={false} />
   </Board>
